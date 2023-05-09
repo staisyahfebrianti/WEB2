5adf8b6a02a183c41b0f5fe06354c26113afe7d4
staisyahfebrianti@gmail.com <staisyahfebrianti@gmail.com>
2023-05-09 12:55:14 +0800

    first commit

---
+++ controllers/DaftarMatakuliahController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace app\controllers;
+
+class DaftarMatakuliahController extends \yii\web\Controller
+{
+    public function actionDelete()
+    {
+        return $this->render('delete');
+    }
+
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+
+    public function actionUpdate()
+    {
+        return $this->render('update');
+    }
+
+    public function actionView()
+    {
+        return $this->render('view');
+    }
+
+}
